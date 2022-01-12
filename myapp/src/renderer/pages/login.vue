0776b2565dd4b49f1644ee8c0428424b330b6fc3
noname <jm080265@gmail.com>
2022-01-12 21:11:53 +0900

    j

---
+++ myapp/src/renderer/pages/login.vue
@@ -0,0 +1,44 @@
+<template>
+ <div>
+  <h1>ログインユーザ</h1>
+  <form @submit.prevent="loginUser">
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input v-model="user.email">
+    </div>
+    <div class="form-group">
+      <label for="password">Password:</label>
+      <input type="password" v-model="user.password">
+    </div>
+    <button type="submit">ログイン</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware({ store, redirect }) {
+    if(store.$auth.loggedIn) {
+      redirect('/');
+    }
+  },
+  data() {
+    return {
+      user:{
+          email:'',
+          password:''
+        }
+    }
+  },
+  methods: {
+    loginUser(){
+        this.$auth.loginWith('local',{
+          data:this.user
+        })
+      },
+  }
+}
+</script>
+
+<style>
+</style>
